6d32992b832c4fc0c88fd5488757fa4dacdae50e
Nicholas Nagle <nnagle@nnagles-air.attlocal.net>
2015-04-04 21:32:57 -0500

    Minimal example with MatrixXd inside rcpp_hello()

---
+++ inst/include/RcppEigenSS.h
@@ -0,0 +1,32 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+//
+// RcppEigenSS.h: Rcpp/Eigen glue
+//
+// Copyright (C)      2011 Douglas Bates, Dirk Eddelbuettel and Romain Francois
+//
+// This file is part of MyEigen.
+//
+// RcppEigenSS is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// RcppEigenSS is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MyEigen.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef RcppEigenSS__RcppEigenSS__h
+#define RcppEigenSS__RcppEigenSS__h
+
+#include <Rcpp.h>
+#include <iterator>
+#include <RcppCommon.h>
+#include <Rconfig.h>
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+
+#endif
